98d5d6fc429521c2f32d03bcb775fa8803cea346
DeeStart <pmdu16063@cusc.ctu.edu.vn>
2021-09-24 22:13:50 +0700

    footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@ let menu = document.querySelector("#menu-btn");
 let navbar = document.querySelector(".navbar");
 const counter = document.querySelectorAll(".counter");
 const speed = 100;
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+const slides = document.querySelectorAll('.slide');
+
 
 menu.onclick = function () {
   menu.classList.toggle("fa-times");
@@ -32,3 +36,61 @@ counter.forEach((counter) => {
 
   updateCount();
 });
+
+var swiper = new Swiper(".box-container", {
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+        slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1440: {
+      slidesPerView: 3,
+    },
+  },
+  loop: true,
+  grabCursor :true,
+});
+
+let index = 0;
+display(index);
+function display (index) {
+	slides.forEach((slide) => {
+		slide.style.display = 'none';
+	});
+	slides[index].style.display = 'flex';
+}
+
+function nextSlide () {
+	index++;
+	if (index > slides.length - 1) {
+		index = 0;
+	}
+	display(index);
+}
+function prevSlide () {
+	index--;
+	if (index < 0) {
+		index = slides.length - 1;
+	}
+	display(index);
+}
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
+
+
+
+
+
+
